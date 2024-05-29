91ca3a12f7fb6a8f2e017189916118e575d0d29f
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-05-29 14:26:12 +0900

    프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tech Blog Hub
+
+- 기술 블로그 모음
+
+---
+
+## ERD(Entity Relationship Diagram)
+
+
+
+## 기술 소개
+
+### Skills
+
+![image](https://github.com/hbkuk/shop/assets/109803585/4c3db82d-f1db-48c7-b622-d52ee1ba68d8)
+
+## 기록
+
+
---
+++ src/test/java/com/techbloghub/techbloghub/TechBlogHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbloghub.techbloghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
