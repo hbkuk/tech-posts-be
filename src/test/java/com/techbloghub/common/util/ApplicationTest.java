3793d09435d3b75daa2d292eb4037a46d70f048f
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-05-30 16:54:02 +0900

    test: 테스트 환경 설정 클래스 추가

---
+++ src/test/java/com/techbloghub/common/util/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.techbloghub.common.util;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(value = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ApplicationTest {
+
+}
