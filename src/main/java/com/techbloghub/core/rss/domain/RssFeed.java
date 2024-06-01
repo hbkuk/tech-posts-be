8ae8241391da25e5da5ad74287fad21e2c3b5a63
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-06-01 13:17:43 +0900

    refactor: Rss Feed 메서드 리팩터링

--- src/main/java/com/techbloghub/core/rss/domain/RssFeed.java
+++ src/main/java/com/techbloghub/core/rss/domain/RssFeed.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @Getter
 @EqualsAndHashCode
@@ -23,24 +24,34 @@ public class RssFeed {
 
     private String description;
 
-    public RssFeed(Item feedItem) {
+    public RssFeed(Item feed) {
+        validateFeed(feed);
+
+        this.link = feed.getLink().get();
+        this.title = feed.getTitle().get();
+        this.publishAt = getPublishDateOrNow(feed.getPubDate());
+        this.description = getDescriptionOrDefault(feed.getDescription());
+    }
+
+    public static RssFeed of(String link, String title, LocalDateTime publishAt, String description) {
+        return new RssFeed(link, title, publishAt, description); // TODO: 정적 팩토리 메서드는 삭제해야겠다..
+    }
+
+    private static void validateFeed(Item feedItem) {
         if (feedItem.getLink().isEmpty()) {
             throw new IllegalArgumentException("피드를 생성하기 위해서는 링크는 반드시 포함되어야 합니다.");
         }
         if (feedItem.getTitle().isEmpty()) {
             throw new IllegalArgumentException("피드를 생성하기 위해서는 타이틀은 반드시 포함되어야 합니다.");
         }
-
-        this.link = feedItem.getLink().get();
-        this.title = feedItem.getTitle().get();
-        this.publishAt = feedItem.getPubDate().isPresent()
-                ? LocalDateTime.parse(DateConverter.convertRfc822ToIso8601(feedItem.getPubDate().get()))
-                : LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-        this.description = feedItem.getDescription().isPresent() ? feedItem.getDescription().get() : "";
     }
 
+    private static String getDescriptionOrDefault(Optional<String> feed) {
+        return feed.orElse("");
+    }
 
-    public static RssFeed of(String link, String title, LocalDateTime publishAt, String description) {
-        return new RssFeed(link, title, publishAt, description); // TODO: 정적 팩토리 메서드는 삭제해야겠다..
+    private static LocalDateTime getPublishDateOrNow(Optional<String> feed) {
+        return feed.map(pubDate -> LocalDateTime.parse(DateConverter.convertRfc822ToIso8601(pubDate)))
+                .orElseGet(() -> LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
     }
 }
