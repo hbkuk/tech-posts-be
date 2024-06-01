2dcf5fb242ab4c36710b6b684ccfcd67f831fa14
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-06-01 15:54:33 +0900

    test: Rss Feed Reader 테스트 코드 추가
    - feat: Rss Feed Reader 테스트 코드에 만족하는 기능 구현

---
+++ src/main/java/com/techbloghub/core/rss/application/RssFeedReader.java
@@ -0,0 +1,36 @@
+package com.techbloghub.core.rss.application;
+
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
+import com.techbloghub.common.exception.common.ErrorType;
+import com.techbloghub.common.exception.server.sub.RssFeedReadException;
+import com.techbloghub.core.rss.domain.RssFeed;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class RssFeedReader {
+
+    private final RssReader rssReader;
+
+    public List<RssFeed> read(String blogRssUrl) {
+        return fetchRssFeedItems(blogRssUrl).map(RssFeed::new).collect(Collectors.toList());
+    }
+
+    private Stream<Item> fetchRssFeedItems(String blogRssUrl) {
+        try {
+            return rssReader.read(blogRssUrl);
+        } catch (IOException e) {
+            log.error("{} - Error reading RSS feed", blogRssUrl, e);
+            throw new RssFeedReadException(ErrorType.RSS_FEED_READ_FAILED);
+        }
+    }
+}
---
+++ src/test/java/com/techbloghub/core/rss/application/RssFeedReaderMockTest.java
@@ -0,0 +1,66 @@
+package com.techbloghub.core.rss.application;
+
+import com.apptasticsoftware.rssreader.DateTime;
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
+import com.techbloghub.core.rss.domain.RssFeed;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.techbloghub.core.blog.domain.Blog.컬리;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@DisplayName("Rss Feed Reader Mock 테스트")
+@ExtendWith(MockitoExtension.class)
+public class RssFeedReaderMockTest {
+
+    RssFeedReader rssFeedReader;
+
+    @Mock
+    RssReader rssReader;
+
+    @BeforeEach
+    void 사전_객체_생성() {
+        rssFeedReader = new RssFeedReader(rssReader);
+    }
+
+    @Nested
+    class 피드_읽기 {
+
+        @Test
+        void 피드_읽기_성공() throws IOException {
+            // given
+            Item 컬리_첫번째_피드_아이템 = new Item(new DateTime());
+            컬리_첫번째_피드_아이템.setTitle("함께 구매하면 좋은 상품이에요! - 장바구니 추천 개발기 2부");
+            컬리_첫번째_피드_아이템.setDescription("보완재 추천 모델을 서빙하기 위한 아키텍처 소개");
+            컬리_첫번째_피드_아이템.setPubDate("Mon, 27 May 2024 10:00:00 +0900");
+            컬리_첫번째_피드_아이템.setLink("http://thefarmersfront.github.io/blog/cart-recommend-model-development_second");
+
+            Item 컬리_두번째_피드_아이템 = new Item(new DateTime());
+            컬리_두번째_피드_아이템.setTitle("함께 구매하면 좋은 상품이에요! - 장바구니 추천 개발기 1부");
+            컬리_두번째_피드_아이템.setDescription("보완재 추천 모델을 적용하고 성과를 거둔 사례 소개");
+            컬리_두번째_피드_아이템.setPubDate("Mon, 20 May 2024 13:00:00 +0900");
+            컬리_두번째_피드_아이템.setLink("http://thefarmersfront.github.io/blog/cart-recommend-model-development");
+
+            when(rssReader.read(컬리.getBlogUrl())).thenReturn(Stream.of(컬리_첫번째_피드_아이템, 컬리_두번째_피드_아이템));
+
+
+            // when
+            List<RssFeed> 읽은_피드 = rssFeedReader.read(컬리.getBlogUrl());
+
+            // then
+
+            assertEquals(List.of(new RssFeed(컬리_첫번째_피드_아이템), new RssFeed(컬리_두번째_피드_아이템)), 읽은_피드);
+        }
+    }
+}
