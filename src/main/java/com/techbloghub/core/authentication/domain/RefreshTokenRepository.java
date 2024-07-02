1d6a8b26da69c0d3db31640a2585e639603bef5b
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-07-02 10:23:20 +0900

    fix: 소셜 로그인 관련 코드 임시 삭제

--- src/main/java/com/techbloghub/core/authentication/domain/RefreshTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.techbloghub.core.authentication.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
