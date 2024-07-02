74c3989e6120b0cee33d471b476a108d26685818
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-07-02 10:30:33 +0900

    fix: 게시글 목록 API 응답 형식 변경으로 인한 기존 테스트 코드 수정

--- src/test/java/com/techbloghub/core/post/acceptance/PostAcceptanceTest.java
+++ src/test/java/com/techbloghub/core/post/acceptance/PostAcceptanceTest.java
@@ -1,22 +1,22 @@
 package com.techbloghub.core.post.acceptance;
 
+import static com.techbloghub.core.blog.domain.Blog.우아한형제들;
+import static com.techbloghub.core.post.step.PostSteps.게시글_목록_요청;
+import static com.techbloghub.core.post.step.PostSteps.게시글_목록_확인;
+import static com.techbloghub.core.post.util.PostTestUtil.게시글_테스트_데이터_생성하기;
+
 import com.techbloghub.common.util.AcceptanceTest;
 import com.techbloghub.core.blog.domain.Blog;
 import com.techbloghub.core.post.domain.Post;
 import com.techbloghub.core.post.domain.PostRepository;
+import com.techbloghub.core.post.step.PostSteps;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-
-import java.util.List;
-
-import static com.techbloghub.core.blog.domain.Blog.우아한형제들;
-import static com.techbloghub.core.post.step.PostSteps.게시글_목록_검증하기;
-import static com.techbloghub.core.post.step.PostSteps.게시글_목록_요청하기;
-import static com.techbloghub.core.post.util.PostTestUtil.게시글_테스트_데이터_생성하기;
 
 @DisplayName("게시글 인수 테스트")
 public class PostAcceptanceTest extends AcceptanceTest {
@@ -28,42 +28,42 @@ public class PostAcceptanceTest extends AcceptanceTest {
     class 게시글_목록 {
 
         /**
-         * given  게시글 테스트 데이터를 데이터베이스에 저장한다.
+         * given  게시글을 등록한다.
          * When   아무 조건 없이 게시글 목록을 요청한다.
-         * Then   기본 설정된 조건에 부합하는 게시글 목록이 응답된다.
+         * Then   기본 설정된 조건으로 선택된 게시글 목록이 응답된다.
          */
         @Test
         void 아무_조건없이_게시글_목록_요청() {
             // given
-            var 저장된_게시글_목록 = 게시글_테스트_데이터_저장하기(우아한형제들, 20);
+            var 저장된_게시글_목록 = 개수만큼_게시글_등록(우아한형제들, 20);
 
             // when
-            var 응답된_게시글_목록 = 게시글_목록_요청하기();
+            var 응답된_게시글_목록 = 게시글_목록_요청();
 
             // then
-            게시글_목록_검증하기(저장된_게시글_목록, 응답된_게시글_목록);
+            게시글_목록_확인(저장된_게시글_목록, 응답된_게시글_목록);
         }
 
         /**
-         * given  게시글 테스트 데이터를 데이터베이스에 저장한다.
+         * given  게시글을 등록한다.
          * And    게시글 목록 요청과 관련된 조건을 설정한다.
-         * When   특정 조건을 포함해서 게시글 목록을 요청한다.
-         * Then   특정 조건에 부합하는 게시글 목록이 응답된다.
+         * When   특정 조건으로 게시글 목록을 요청한다.
+         * Then   특정 조건으로 선택된 게시글 목록이 응답된다.
          */
         @Test
         void 페이지_정보를_포함한_게시글_목록_요청() {
             // given
-            var 저장된_게시글_목록 = 게시글_테스트_데이터_저장하기(우아한형제들, 100);
+            var 저장된_게시글_목록 = 개수만큼_게시글_등록(우아한형제들, 100);
             var 페이지_요청_정보 = PageRequest.of(1, 5, Sort.Direction.DESC, "publishAt");
 
             // when
-            var 응답된_게시글_목록 = 게시글_목록_요청하기(페이지_요청_정보);
+            var 응답된_게시글_목록 = PostSteps.게시글_목록_요청(페이지_요청_정보);
 
             // then
-            게시글_목록_검증하기(페이지_요청_정보, 저장된_게시글_목록, 응답된_게시글_목록);
+            게시글_목록_확인(페이지_요청_정보, 저장된_게시글_목록, 응답된_게시글_목록);
         }
 
-        private List<Post> 게시글_테스트_데이터_저장하기(Blog 블로그, int 개수) {
+        private List<Post> 개수만큼_게시글_등록(Blog 블로그, int 개수) {
             List<Post> 생성된_게시글_데이터 = 게시글_테스트_데이터_생성하기(블로그, 개수);
             return postRepository.saveAll(생성된_게시글_데이터);
         }
--- src/test/java/com/techbloghub/core/post/step/PostSteps.java
+++ src/test/java/com/techbloghub/core/post/step/PostSteps.java
@@ -1,35 +1,34 @@
 package com.techbloghub.core.post.step;
 
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techbloghub.core.post.domain.Post;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 
 public class PostSteps {
 
     public static final int 페이지_기본_사이즈 = 10;
 
-    public static ExtractableResponse<Response> 게시글_목록_요청하기() {
+    public static ExtractableResponse<Response> 게시글_목록_요청() {
         return given().log().all()
-                .when().log().all()
-                .get("/posts")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
+            .when().log().all()
+            .get("/api/posts")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 
-    public static ExtractableResponse<Response> 게시글_목록_요청하기(PageRequest 페이지_요청_정보) {
+    public static ExtractableResponse<Response> 게시글_목록_요청(PageRequest 페이지_요청_정보) {
         Map<String, String> 파라미터_목록 = new HashMap<>();
         파라미터_목록.put("size", String.valueOf(페이지_요청_정보.getPageSize()));
         파라미터_목록.put("page", String.valueOf(페이지_요청_정보.getPageNumber()));
@@ -40,37 +39,39 @@ public class PostSteps {
         파라미터_목록.put("sort", sortParam);
 
         return given().log().all()
-                .params(파라미터_목록)
-                .when().log().all()
-                .get("/posts")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
+            .params(파라미터_목록)
+            .when().log().all()
+            .get("/api/posts")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 
-    public static void 게시글_목록_검증하기(List<Post> 저장된_게시글_목록, ExtractableResponse<Response> 응답된_게시글_목록) {
+    public static void 게시글_목록_확인(List<Post> 저장된_게시글_목록, ExtractableResponse<Response> 응답된_게시글_목록) {
         List<String> 예상하는_응답된_게시글_제목_목록 = 저장된_게시글_목록.stream()
-                .sorted(Comparator.comparing(Post::getPublishAt).reversed())
-                .limit(페이지_기본_사이즈)
-                .map(Post::getTitle)
-                .collect(Collectors.toList());
+            .sorted(Comparator.comparing(Post::getPublishAt).reversed())
+            .limit(페이지_기본_사이즈)
+            .map(Post::getTitle)
+            .collect(Collectors.toList());
 
-        List<String> 응답된_게시글_제목_목록 = 게시글_제목만_분류하기(응답된_게시글_목록);
+        List<String> 응답된_게시글_제목_목록 = 게시글_제목만_분류(응답된_게시글_목록);
         assertThat(응답된_게시글_제목_목록).isEqualTo(예상하는_응답된_게시글_제목_목록);
     }
 
-    public static List<String> 게시글_제목만_분류하기(ExtractableResponse<Response> 게시글_목록_응답) {
-        return 게시글_목록_응답.jsonPath().getList("title", String.class);
+    public static List<String> 게시글_제목만_분류(ExtractableResponse<Response> 게시글_목록_응답) {
+        return 게시글_목록_응답.jsonPath().getList("items.title", String.class);
     }
 
-    public static void 게시글_목록_검증하기(PageRequest 페이지_요청_정보, List<Post> 저장된_게시글_목록, ExtractableResponse<Response> 응답된_게시글_목록) {
+    public static void 게시글_목록_확인(PageRequest 페이지_요청_정보, List<Post> 저장된_게시글_목록,
+        ExtractableResponse<Response> 응답된_게시글_목록) {
         var 예상하는_응답된_게시글_제목_목록 = 저장된_게시글_목록.stream()
-                .sorted(Comparator.comparing(Post::getPublishAt).reversed())
-                .skip(5) // TODO: 페이지 계산 필요
-                .limit(페이지_요청_정보.getPageSize())
-                .map(Post::getTitle)
-                .collect(Collectors.toList());
-        var 응답된_게시글_제목_목록 = 게시글_제목만_분류하기(응답된_게시글_목록);
+            .sorted(Comparator.comparing(Post::getPublishAt).reversed())
+            .skip(5) // TODO: 페이지 계산 필요
+            .limit(페이지_요청_정보.getPageSize())
+            .map(Post::getTitle)
+            .collect(Collectors.toList());
+        var 응답된_게시글_제목_목록 = 게시글_제목만_분류(응답된_게시글_목록);
+
         assertThat(응답된_게시글_제목_목록).isEqualTo(예상하는_응답된_게시글_제목_목록);
     }
 }
