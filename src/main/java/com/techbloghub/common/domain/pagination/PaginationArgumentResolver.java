a967147da2f5ead05ed31cfe99bfcbeba4a52369
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-07-22 20:25:45 +0900

    fix: 불필요한 매개변수 삭제

--- src/main/java/com/techbloghub/common/domain/pagination/PaginationArgumentResolver.java
+++ src/main/java/com/techbloghub/common/domain/pagination/PaginationArgumentResolver.java
@@ -3,6 +3,7 @@ package com.techbloghub.common.domain.pagination;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
@@ -19,7 +20,7 @@ public class PaginationArgumentResolver implements HandlerMethodArgumentResolver
     public PaginationRequest resolveArgument(MethodParameter parameter,
         ModelAndViewContainer mavContainer,
         NativeWebRequest webRequest,
-        org.springframework.web.bind.support.WebDataBinderFactory binderFactory) {
+        WebDataBinderFactory binderFactory) {
         PaginationDefault paginationDefault = parameter.getParameterAnnotation(
             PaginationDefault.class);
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
