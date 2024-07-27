01cd856d7b0e38ae7590ad945fc6aea38c0873d7
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-07-27 15:15:55 +0900

    fix: 서브쿼리에서 publicshAt을 최대 값으로 선택하고, 게시글 번호가 가장 큰 게시물을 필터링 하도록 쿼리 재구성

--- src/main/java/com/techbloghub/core/post/domain/PostRepository.java
+++ src/main/java/com/techbloghub/core/post/domain/PostRepository.java
@@ -9,6 +9,15 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT post FROM Post post WHERE post.blog = :blog AND post.publishAt = (SELECT MAX(p.publishAt) FROM Post p WHERE p.blog = :blog)")
+    @Query("SELECT post FROM Post post " +
+        "WHERE post.blog = :blog " +
+        "AND post.publishAt = (SELECT MAX(p.publishAt) " +
+        "FROM Post p " +
+        "WHERE p.blog = :blog) " +
+        "AND post.id = (SELECT MAX(p2.id) " +
+        "FROM Post p2 " +
+        "WHERE p2.blog = :blog " +
+        "AND p2.publishAt = post.publishAt)")
     Optional<Post> findLatestPost(@Param("blog") Blog blog);
+
 }
