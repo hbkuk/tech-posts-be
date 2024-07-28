75b97b827e93b70aff5190d1e40cec55f6fd80cf
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-07-28 14:53:17 +0900

    feat: ZUM 기술 블로그 추가

--- src/main/java/com/techbloghub/core/blog/domain/Blog.java
+++ src/main/java/com/techbloghub/core/blog/domain/Blog.java
@@ -15,7 +15,8 @@ public enum Blog {
     컬리("컬리", "KURLY", "https://helloworld.kurly.com/feed.xml"),
     인프랩("인프랩", "INFLAB", "https://tech.inflab.com/rss.xml"),
     우아한형제들("우아한형제들", "WOOWAHAN", "https://techblog.woowahan.com/feed/"),
-    요기요("요기요", "YOGIYO", "https://medium.com/feed/deliverytechkorea");
+    요기요("요기요", "YOGIYO", "https://medium.com/feed/deliverytechkorea"),
+    줌("줌", "ZUM", "https://zuminternet.github.io/feed.xml");
 
     private final String koreanName;
     private final String englishName;
